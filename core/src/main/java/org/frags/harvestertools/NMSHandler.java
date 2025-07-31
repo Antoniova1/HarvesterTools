9f3df7c69ab1a40ec2d33f909e69351af704fd87
I AM DEATH <108409110+Antoniova1@users.noreply.github.com>
2025-07-31 01:38:20 -0600

    Update NMSHandler.java

--- core/src/main/java/org/frags/harvestertools/NMSHandler.java
+++ core/src/main/java/org/frags/harvestertools/NMSHandler.java
@@ -1,30 +1,17 @@
-package org.frags.harvestertools.nms_1_21_4;
+package org.frags.harvestertools;
 
 import org.bukkit.block.Block;
 import org.bukkit.entity.FishHook;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.frags.harvestertools.HarvesterTools;
-import org.frags.harvestertools.NMSHandler;
 
-import java.util.Collections;
 import java.util.List;
 
-public class NMSHandlerImpl implements NMSHandler {
+public interface NMSHandler {
 
-    @Override
-    public void replenishCrop(Block block, HarvesterTools plugin) {
-        // Implementa según sea necesario
-    }
+    void replenishCrop(Block block, HarvesterTools plugin);
+    List<ItemStack> getDrops(Block block);
 
-    @Override
-    public List<ItemStack> getDrops(Block block) {
-        // Implementa según sea necesario
-        return Collections.emptyList();
-    }
+    void regenBlock(Block block, HarvesterTools plugin);
 
-    @Override
-    public void regenBlock(Block block, HarvesterTools plugin) {
-        // Implementa según sea necesario
-    }
 }
